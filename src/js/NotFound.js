536ef25689d3ad0e34c69adec58ef8bc6091c00e
eyadbinamir <eyad.y.binamir@gmail.com>
2023-04-12 04:33:56 +0200

    .

--- src/js/NotFound.js
+++ src/js/NotFound.js
@@ -4,33 +4,34 @@ import error from "../imgs/404.png";
 import "../css/NotFound.css";
 import { useContext } from "react";
 const NotFound = () => {
-	const context = useContext(Global);
-	const english = context.language === "english";
-	const arabic = context.language === "arabic";
-	return (
-		<div className="py-5 container mt-0 mb-0 m-auto not-found ">
-			<div className="row align-items-center justify-content-center">
-				<div className="col-12 col-md-6 ">
-					<img src={error} alt="Page not found" className="img-fluid" />
-				</div>
-				<div
-					dir={english ? "ltr" : arabic ? "rtl" : ""}
-					className="col-12 col-md-6 d-flex flex-column align-items-center d-md-block ">
-					<h2 className="mb-5 mt-5 justify-content-center d-flex justify-content-md-start">
-						{english
-							? "Error 404.page is not found."
-							: arabic
-							? "خطأ 404. الصحفة غير موجودة"
-							: ""}
-					</h2>
-					<Link to="/">
-						<button className={`p-2 ${context.theme}`}>
-							{english ? "Go to Home" : arabic ? "الصّفحة الرئيسية" : ""}
-						</button>
-					</Link>
-				</div>
-			</div>
-		</div>
-	);
+  const context = useContext(Global);
+  const english = context.language === "english";
+  const arabic = context.language === "arabic";
+  return (
+    <div className="py-5 container mt-0 mb-0 m-auto not-found ">
+      <div className="row align-items-center justify-content-center">
+        <div className="col-12 col-md-6 ">
+          <img src={error} alt="Page not found" className="img-fluid" />
+        </div>
+        <div
+          dir={english ? "ltr" : arabic ? "rtl" : ""}
+          className="col-12 col-md-6 d-flex flex-column align-items-center d-md-block "
+        >
+          <h2 className="mb-5 mt-5 justify-content-center d-flex justify-content-md-start">
+            {english
+              ? "Error 404.page is not found."
+              : arabic
+              ? "خطأ 404. الصحفة غير موجودة."
+              : ""}
+          </h2>
+          <Link to="/">
+            <button className={`p-2 ${context.theme}`}>
+              {english ? "Go to Home" : arabic ? "الصّفحة الرئيسية" : ""}
+            </button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 export default NotFound;
